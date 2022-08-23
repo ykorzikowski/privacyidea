20aa441568ed40f2601818e753484b6a8caa8c5c
Yannik Korzikowski <work@korzikowski.de>
2022-08-23 10:10:27 +0200

    make it work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-WORKERS=${PI_WORKERS:4}
-PI_CFG=/config/pi.cfg
+set -eux
+
+WORKERS=${PI_WORKERS:=4}
+export PI_CFG=/etc/privacyidea/pi.cfg
+export PI_LOGFILE=/logs/privacyidea.log
 source bin/activate
 
 if [ ! -f ${PI_CFG} ]; then
@@ -9,8 +12,11 @@ if [ ! -f ${PI_CFG} ]; then
   echo "PI_PEPPER = '$PEPPER'" >> ${PI_CFG}
   SECRET="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c24)"
   echo "SECRET_KEY = '$SECRET'" >> ${PI_CFG}
+  echo "PI_LOGFILE = '$PI_LOGFILE'" >> ${PI_CFG}
+  echo "PI_AUDIT_SQL_TRUNCATE = True" >> ${PI_CFG}
+  echo "PI_LOGLEVEL = 20" >> ${PI_CFG}
 
   echo ${SQLALCHEMY_DATABASE_URI} >> ${PI_CFG}
 fi
 
-gunicorn --workers=${WORKERS} privacyidea:app
+gunicorn -b "0.0.0.0:8000" --workers=${WORKERS} "privacyidea.app:create_app()"
